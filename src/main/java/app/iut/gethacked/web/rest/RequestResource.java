c8a6f5b3fd102b24383d41a205da7e6b39b1dbc4
Allan <allan.soule@outlook.fr>
2019-05-21 10:45:50 +0200

    REST v2

--- src/main/java/app/iut/gethacked/web/rest/RequestResource.java
+++ src/main/java/app/iut/gethacked/web/rest/RequestResource.java
@@ -89,6 +89,19 @@ public class RequestResource {
     }
 
     /**
+     * GET  /requests/:id : get the "id" request.
+     *
+     * @param id the id of the request to retrieve
+     * @return the ResponseEntity with status 200 (OK) and with body the request, or with status 404 (Not Found)
+     */
+    @GetMapping("/requests/{id}")
+    public ResponseEntity<Request> getRequest(@PathVariable Long id) {
+        log.debug("REST request to get Request : {}", id);
+        Optional<Request> request = requestRepository.findById(id);
+        return ResponseUtil.wrapOrNotFound(request);
+    }
+
+    /**
      * GET  /requests : get all the requests.
      *
      * @return the ResponseEntity with status 200 (OK) and the list of requests in body
@@ -111,26 +124,13 @@ public class RequestResource {
                     Predicate p = cb.equal(root.get(Request_.title), search.title);
                     res = (res == null) ? p : cb.and(res,p);
                 }
-                    return res;
+                return res;
 
             }
         };
 
         List<Request> requests = requestRepository.findAll(spec);
         return requests;
-    }
-
-    /**
-     * GET  /requests/:id : get the "id" request.
-     *
-     * @param id the id of the request to retrieve
-     * @return the ResponseEntity with status 200 (OK) and with body the request, or with status 404 (Not Found)
-     */
-    @GetMapping("/requests/{id}")
-    public ResponseEntity<Request> getRequest(@PathVariable Long id) {
-        log.debug("REST request to get Request : {}", id);
-        Optional<Request> request = requestRepository.findById(id);
-        return ResponseUtil.wrapOrNotFound(request);
     }
 
     /**
